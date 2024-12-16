49bbd65294c359dab3dcaf5c1bd60ccf88f13069
safae <125465623+sh15935@users.noreply.github.com>
2024-12-16 02:06:38 +0100

    Update ClientUDP.py

--- TCP_UDP/UDP/ClientUDP.py
+++ TCP_UDP/UDP/ClientUDP.py
@@ -5,7 +5,7 @@ def start_udp_client():
     port = 5001
 
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    message = "Hello, UDP Server!"
+    message = input()
     client_socket.sendto(message.encode(), (host, port))
     response, _ = client_socket.recvfrom(1024)
     print(f"Server Response: {response.decode()}")
